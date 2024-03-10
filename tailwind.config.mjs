d3c7a6e2ba14271f1a4ff38412fba3112a6d30fa
Coollbreackerz <ralberinfante4@gmail.com>
2024-03-10 15:28:42 -0400

    final hero pc and best section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,17 @@ export default {
 			fontFamily:{
 				'tock':['Fira Code Variable'],
 				'libre':['Libre Baskerville']
+			},
+			colors:{
+				'antiflash-white':'#F0F0F0ff',
+				'dim-gray':'#5B6167ff',
+				'french-gray':'#AAB3BBff',
+				'raw-umber':'#966B42ff',
+				'onyx':'#36393Dff',
+				'eerie-black':'#1E242Aff',
+				'eerie-black-2':'#18181Bff',
+				'cinereous':'#9E928Bff',
+				'charcoal':'#354551ff',
 			}
 		},
 	},
