6245cacc6f2a6d7fc7c6d88a486a76c60cd433c5
Coollbreackerz <ralberinfante4@gmail.com>
2024-03-17 22:08:24 -0400

    ref all web

--- src/models/Areas.ts
+++ src/models/Areas.ts
@@ -1,10 +1,14 @@
 import type ListModel from "./ListModel";
 
 export default class Areas{
-  constructor(title:String,elements:ListModel[]){
+  constructor(title:String,elements:ListModel[],tag:String,description:String){
     this.Title = title;
     this.Services = elements;
+    this.Tag = tag;
+    this.Description = description;
   }
-    Title:String;
-    Services:ListModel[]
+  Description: String;
+  Title:String;
+  Services:ListModel[];
+  Tag:String;
 }
